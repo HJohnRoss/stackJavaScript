bc74ae58eadf9f784efda3fc45e645eb28f73c36
FJohnHRoss <rossjohn936@gmail.com>
2023-01-06 16:15:13 -0800

    super ninja

--- OOP/Ninja/ninja.js
+++ OOP/Ninja/ninja.js
@@ -17,3 +17,24 @@ const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
+
+class Sensei extends Ninja {
+    constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10){
+        super(name, health, speed, strength);
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+        this.wisdom = wisdom;
+    }
+
+    speakWisdom = () => {
+        this.drinkSake();
+        console.log(`What one programmer can do in one month, two programmers can do in two months.`)
+    }
+}
+
+console.log("====================")
+const sensei1 = new Sensei("John", 410, 21, 50, 1000);
+sensei1.speakWisdom();
+sensei1.showStats();
