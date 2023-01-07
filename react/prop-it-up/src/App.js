9692955eb2b4da17c2773f76b8104a0bd79e288d
FJohnHRoss <rossjohn936@gmail.com>
2023-01-06 19:11:13 -0800

    prop it up

---
+++ react/prop-it-up/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import ReactComponent from './components/ReactComponent';
+
+function App() {
+    return (
+        <div>
+            <ReactComponent firstName={"Jane"} hairColor={"Black"} age={"45"} lastName={"Doe"}/>
+            <ReactComponent firstName={"John"} hairColor={"Brown"} age={"88"} lastName={"Smith"}/>
+            <ReactComponent firstName={"Millard"} hairColor={"Brown"} age={"50"} lastName={"Fillmore"}/>
+            <ReactComponent firstName={"Maria"} hairColor={"Brown"} age={"62"} lastName={"Smith"}/>
+        </div>
+    );
+}
+
+export default App;
