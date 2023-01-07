ab35feee598c7277b6c0eb58db3d73ea6241a005
FJohnHRoss <rossjohn936@gmail.com>
2023-01-06 18:42:35 -0800

    fisrt Jsx

---
+++ react/firstjsx/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import MyNewComponent from './components/MyNewComponent';
+
+function App() {
+    return (
+        <MyNewComponent/>
+    );
+}
+
+export default App;
