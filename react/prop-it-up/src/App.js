367360a7b4941c62ec365866197a26f948605bc5
FJohnHRoss <rossjohn936@gmail.com>
2023-01-06 20:04:36 -0800

    putting it together

--- react/prop-it-up/src/App.js
+++ react/prop-it-up/src/App.js
@@ -6,10 +6,10 @@ import ReactComponent from './components/ReactComponent';
 function App() {
     return (
         <div>
-            <ReactComponent firstName={"Jane"} hairColor={"Black"} age={"45"} lastName={"Doe"}/>
-            <ReactComponent firstName={"John"} hairColor={"Brown"} age={"88"} lastName={"Smith"}/>
-            <ReactComponent firstName={"Millard"} hairColor={"Brown"} age={"50"} lastName={"Fillmore"}/>
-            <ReactComponent firstName={"Maria"} hairColor={"Brown"} age={"62"} lastName={"Smith"}/>
+            <ReactComponent firstName={"Jane"} hairColor={"Black"} age={45} lastName={"Doe"}/>
+            <ReactComponent firstName={"John"} hairColor={"Brown"} age={88} lastName={"Smith"}/>
+            <ReactComponent firstName={"Millard"} hairColor={"Brown"} age={50} lastName={"Fillmore"}/>
+            <ReactComponent firstName={"Maria"} hairColor={"Brown"} age={62} lastName={"Smith"}/>
         </div>
     );
 }
--- react/prop-it-up/src/components/ReactComponent.jsx
+++ react/prop-it-up/src/components/ReactComponent.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class ReactComponent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            age : this.props.age
+        }
+    }
+
+    incAge = () => {
+        this.setState({age : this.state.age += 1})
+    }
+
     render() {
         const {firstName, lastName, age, hairColor} = this.props;
         return (
             <div>
                 <h1>{ this.props.lastName }, { this.props.firstName }</h1>
-                <p>Age: { this.props.age }</p>
+                <p>Age: { this.state.age }</p>
                 <p>Hair Color: { this.props.hairColor }</p>
+                <button onClick={this.incAge}>Birthday button for { this.props.firstName} { this.props.lastName }</button>
             </div>
         );
     }
