f5f7c0ac6374d6aa9bb8dd7918d08a881bdceceb
FJohnHRoss <rossjohn936@gmail.com>
2023-01-12 09:25:59 -0800

    algo

--- algo/w2d4.js
+++ algo/w2d4.js
@@ -13,22 +13,22 @@ const partition = (arr) => {
 }
 
 
-// const partition = (arr) => {
-//     let pivet = 0
-//     for (let i = 1; i < arr.length; i++) {
-//         if (arr[i] <= arr[pivet]) {
-//             let tempi = i
-//             while (tempi > pivet) {
-//                 let temp = arr[tempi]
-//                 arr[tempi] = arr[tempi - 1]
-//                 arr[tempi - 1] = temp
-//                 tempi--
-//             }
-//             pivet++
-//         }
-//     }
-//     return arr
-// }
+const partition1 = (arr) => {
+    let pivet = 0
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] <= arr[pivet]) {
+            let tempi = i
+            while (tempi > pivet) {
+                let temp = arr[tempi]
+                arr[tempi] = arr[tempi - 1]
+                arr[tempi - 1] = temp
+                tempi--
+            }
+            pivet++
+        }
+    }
+    return arr
+}
 
 
 console.log(partition([5, 4, 9, 2, 5, 3]))
