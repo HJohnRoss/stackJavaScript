f5aa165cf37787501550fd167c97ac70cd9f0886
FJohnHRoss <rossjohn936@gmail.com>
2023-01-22 19:37:42 -0800

    test

--- algo/w2d5.js
+++ algo/w2d5.js
@@ -8,13 +8,9 @@ const findConsecutiveSums = (nums, targetSum) => {
   let returnArr = []
   let math = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] != 0) {
-      math = nums[i]
-    }
-    for (let j = i; j < nums.length; j++) {
-      if (i != j && nums[j] != 0) {
-        math += nums[j]
-      }
+    math = nums[i]
+    for (let j = i + 1; j < nums.length; j++) {
+      math += nums[j]
       if (math > targetSum) {
         tempArr = []
         break
@@ -33,7 +29,7 @@ const findConsecutiveSums = (nums, targetSum) => {
 
 const nums1 = [2, 5, 3, 6, 7, 23, 12];
 const sum1 = 16;
-console.log(findConsecutiveSums(nums1,sum1));
+console.log(findConsecutiveSums(nums1, sum1));
 // const expected1 = [
 //   [2, 5, 3, 6],
 //   [3, 6, 7],
@@ -41,12 +37,12 @@ console.log(findConsecutiveSums(nums1,sum1));
 
 const nums2 = [];
 const sum2 = 5;
-console.log(findConsecutiveSums(nums2,sum2));
+console.log(findConsecutiveSums(nums2, sum2));
 // const expected2 = [];
 
 const nums3 = [10, 15, 20, 35, 30];
 const sum3 = 5;
-console.log(findConsecutiveSums(nums3,sum3));
+console.log(findConsecutiveSums(nums3, sum3));
 // const expected3 = [];
 
 // Bonus:
